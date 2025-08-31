91424456815654261ef0bb2f0e8432b852658ada
Aayush Kumar Singh <aayushkr3003@gmail.com>
2025-08-31 23:57:19 +0530

    Update Q2.cpp

--- Assignment2/Q2.cpp
+++ Assignment2/Q2.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int som[],int size){
+void bubbleSort(int arr[],int size){
     for (int i=0;i<size;i++){
         for (int j=i+1;j<size;j++){
-            if(som[i]>som[j]){
-                int temp=som[i];
-                som[i]=som[j];
-                som[j]=temp;
+            if(arr[i]>arr[j]){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
             }
         }
     }
@@ -15,10 +15,10 @@ void bubbleSort(int som[],int size){
 }
 
 int main(){
-    int som[]={64,34,25,12,22,11,90};
-    bubbleSort(som,6);
+    int arr[]={64,34,25,12,22,11,90};
+    bubbleSort(arr,6);
     for (int i=0;i<6;i++){
-        cout<<som[i]<<endl;
+        cout<<arr[i]<<endl;
     }
 
 }
